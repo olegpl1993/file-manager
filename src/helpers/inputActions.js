e114bbbd3425b8e9706d1bb8c78ec2a421ee162c
olegpl1993 <olegpl1993@gmail.com>
2023-06-24 00:21:35 +0300

    feat: create currentDirectory

--- src/helpers/inputActions.js
+++ src/helpers/inputActions.js
@@ -1,15 +1,14 @@
 import { actionsList } from "./actionsList.js";
 
-// обработчик строки ввода
 export const inputActions = (input) => {
   try {
-    const [actionName, ...args] = input.trim().split(" ");
-    const joinedArgs = args.join(" ");
+    const [actionName, ...args] = input.trim().split(" "); // получаем имя действия и списка аргументов
+    const joinedArgs = args.join(" "); // объединяем аргументы
     console.log("test action:", actionName);
     console.log("test args:", joinedArgs);
-    const action = actionsList[actionName];
-    action();
+    const action = actionsList[actionName]; // получаем действие
+    action(); // вызываем действие
   } catch (error) {
-    console.log("Invalid input");
+    console.log("Invalid input"); // в случае не корректного ввода выводим сообщение
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import readline from "readline";
 import { getUsername } from "./helpers/getUsername.js";
 import { inputActions } from "./helpers/inputActions.js";
+import os from "os";
 
 // npm run start -- --username=Oleg
 
+const username = getUsername(); // получаем имя пользователя
+const homeDirectory = os.homedir(); // получаем директорию пользователя
+export let currentDirectory = homeDirectory; // текущая директория
+
 const app = () => {
-  const username = getUsername(); // получаем имя пользователя
-  console.log(`Welcome to the File Manager, ${username}!`); // выводим приветствие
+  console.log(`Welcome to the File Manager, ${username}!`);
+  console.log(`You are currently in ${homeDirectory}`);
 
   // интерфейс для чтения ввода из консоли
   const rl = readline.createInterface({
@@ -17,10 +22,11 @@ const app = () => {
   // обработчик ввода
   rl.on("line", (input) => {
     inputActions(input);
+    console.log(`You are currently in ${currentDirectory}`);
   });
 
   // обработчик завершения работы
-  process.on('exit', () => {
+  process.on("exit", () => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
   });
 };
