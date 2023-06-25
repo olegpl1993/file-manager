d39ac25bb46dd9b4431aebbfd14def10126eb23c
olegpl1993 <olegpl1993@gmail.com>
2023-06-25 19:46:43 +0300

    feat: rn

--- src/helpers/actionsList.js
+++ src/helpers/actionsList.js
@@ -7,8 +7,12 @@ export const actionsList = {
     process.exit(0);
   },
   up: () => {
-    const upDirectory = path.join(currentDirectory, "..");
-    setCurrentDirectory(upDirectory);
+    try {
+      const upDirectory = path.join(currentDirectory, "..");
+      setCurrentDirectory(upDirectory);
+    } catch {
+      console.log(`Operation failed`);
+    }
   },
   cd: async (joinedArgs) => {
     try {
@@ -24,39 +28,53 @@ export const actionsList = {
     }
   },
   ls: async () => {
-    const files = await fs.promises.readdir(currentDirectory, {
-      withFileTypes: true,
-    });
-    const fileStats = files.map((file) => {
-      return {
-        Name: file.name,
-        Type: file.isDirectory() ? "directory" : "file",
-      };
-    });
-    // Сортировка по типу (в алфавитном порядке) и по имени (в алфавитном порядке)
-    fileStats.sort((a, b) => {
-      if (a.Type === b.Type) {
-        return a.Name.localeCompare(b.Name); // Сортировка по имени
-      } else {
-        return a.Type.localeCompare(b.Type); // Сортировка по типу
-      }
-    });
-    console.table(fileStats);
+    try {
+      const files = await fs.promises.readdir(currentDirectory, {
+        withFileTypes: true,
+      });
+      const fileStats = files.map((file) => {
+        return {
+          Name: file.name,
+          Type: file.isDirectory() ? "directory" : "file",
+        };
+      });
+      // Сортировка по типу (в алфавитном порядке) и по имени (в алфавитном порядке)
+      fileStats.sort((a, b) => {
+        if (a.Type === b.Type) {
+          return a.Name.localeCompare(b.Name); // Сортировка по имени
+        } else {
+          return a.Type.localeCompare(b.Type); // Сортировка по типу
+        }
+      });
+      console.table(fileStats);
+    } catch {
+      console.log(`Operation failed`);
+    }
   },
   cat: async (joinedArgs) => {
-    const pathToFile = path.join(currentDirectory, joinedArgs);
     try {
+      const pathToFile = path.join(currentDirectory, joinedArgs);
       const data = await fs.promises.readFile(pathToFile, "utf8");
       console.log(data);
-    } catch (error) {
+    } catch {
       console.log(`Operation failed`);
     }
   },
   add: async (joinedArgs) => {
-    const pathToFile = path.join(currentDirectory, joinedArgs);
     try {
+      const pathToFile = path.join(currentDirectory, joinedArgs);
       await fs.promises.writeFile(pathToFile, "", "utf8");
-    } catch (error) {
+    } catch {
+      console.log(`Operation failed`);
+    }
+  },
+  rn: async (joinedArgs) => {
+    try {
+      const [arg1, arg2] = joinedArgs.split(" ", 2);
+      const pathToFile = path.join(currentDirectory, arg1);
+      const pathToRenamedFile = path.join(currentDirectory, arg2);
+      await fs.promises.rename(pathToFile, pathToRenamedFile);
+    } catch {
       console.log(`Operation failed`);
     }
   },
